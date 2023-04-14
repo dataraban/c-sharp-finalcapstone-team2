9fcbc850335b4c61e5547de674e12468be82a077
Darya Taraban <daryastaraban@gmail.com>
2023-04-14 16:29:39 -0400

    back end controllers and daos for favorites

---
+++ dotnet/Capstone/Controllers/FavoritesController .cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Capstone.DAO;
+using Capstone.Models;
+using Capstone.Security;
+using System;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FavoritesController : ControllerBase
+    {
+        private readonly IFavoritesDao favoritesDao;
+
+        public FavoritesController(IFavoritesDao _favoritesDao)
+        {
+            favoritesDao = _favoritesDao;
+        }
+
+        [HttpPost("/userFavorites")]//is this the right route? Please check
+        public IActionResult AddUserMovie(UserMovie userMovie)
+        {
+            //IActionResult result;
+
+            //favoritesDao.AddFavoriteMovie(userMovie.UserId, userMovie.MovieId);
+
+            //return result;
+
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ dotnet/Capstone/DAO/IFavoritesDao.cs
@@ -0,0 +1,13 @@
+﻿using Capstone.Models;
+using System.Collections.Generic;
+
+namespace Capstone.DAO
+{
+    public interface IFavoritesDao
+    {
+        IList<UserMovie> GetFavoriteMovies(int userId);
+        UserMovie AddFavoriteMovie(int userId, int movieId);
+        IList<UserGenre> GetFavoriteGenres(int userId);
+        UserGenre AddFavoriteGenre(int userId, int genreId);
+    }
+}
